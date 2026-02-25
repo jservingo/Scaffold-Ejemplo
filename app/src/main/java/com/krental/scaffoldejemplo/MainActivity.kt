fb5e92a4a54ab8306765a5a0e87d4016eb1c99ff
Jorge Servin <jservingo@gmail.com>
2026-02-25 15:50:19 -0400

    TopAppBar

--- app/src/main/java/com/krental/scaffoldejemplo/MainActivity.kt
+++ app/src/main/java/com/krental/scaffoldejemplo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.krental.scaffoldejemplo
 
+import android.R.attr.onClick
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,9 +8,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -28,9 +37,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldEjemplo() {
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text="Mi aplicacion")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { }
+                    ){
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "Menu"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = { /* Accion de busqueda */ },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = "Buscar"
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        },
         containerColor = MaterialTheme.colorScheme.primaryContainer,
         content = { paddingValues ->
             //Contenido principal
