74219ccdd6749e0a11edf3c137857d496ed48292
Jorge Servin <jservingo@gmail.com>
2026-02-26 14:15:13 -0400

    NavigationBar

--- app/src/main/java/com/krental/scaffoldejemplo/MainActivity.kt
+++ app/src/main/java/com/krental/scaffoldejemplo/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.krental.scaffoldejemplo
 
 import android.R.attr.onClick
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
@@ -17,6 +22,7 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.BottomAppBar
@@ -28,11 +34,18 @@ import androidx.compose.material3.FloatingActionButtonElevation
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -54,6 +67,10 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldEjemplo() {
+    val items = listOf("Inicio","Buscar","Perfil")
+    val icons = listOf(Icons.Default.Home,Icons.Default.Search,Icons.Default.Person)
+    var selectedItem by rememberSaveable { mutableIntStateOf(0) }
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -98,6 +115,33 @@ fun ScaffoldEjemplo() {
             )
         },
         bottomBar = {
+            NavigationBar(
+                modifier = Modifier.fillMaxWidth(),
+                containerColor = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                tonalElevation = 3.dp,
+                windowInsets = NavigationBarDefaults.windowInsets
+            ){
+                items.forEachIndexed { index, label ->
+                    NavigationBarItem(
+                        onClick = { selectedItem = index },
+                        selected = selectedItem == index,
+                        icon = {
+                            Icon(
+                                imageVector = icons[index],
+                                contentDescription = label
+                            )
+                        },
+                        label = {
+                            Text(text = label)
+                        },
+                        alwaysShowLabel = true
+                    )
+                }
+            }
+            //No podemos utilizar un ButtonAppBar y un NavigationBar
+            //Solamente podemos implementar uno de ellos
+            /*
             BottomAppBar(
                 actions = {
                     IconButton(onClick={/* Accion de inicio */}){
@@ -127,6 +171,7 @@ fun ScaffoldEjemplo() {
                 containerColor = MaterialTheme.colorScheme.surface,
                 contentColor = MaterialTheme.colorScheme.onSurface
             )
+             */
         },
         floatingActionButton = {
             FloatingActionButton( //Small - Large
@@ -154,7 +199,11 @@ fun ScaffoldEjemplo() {
                     .padding(paddingValues),
                 contentAlignment = Alignment.Center
             ) {
-                Text(text="Contenido principal")
+                Column() {
+                    Text(text = "Contenido")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Pantalla ${items[selectedItem]}")
+                }
             }
         }
     )
