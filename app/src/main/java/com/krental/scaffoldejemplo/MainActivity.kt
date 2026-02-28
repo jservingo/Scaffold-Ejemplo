70b45aa6a11cc143b5be8a2f1365a90609b79749
Jorge Servin <jservingo@gmail.com>
2026-02-27 21:07:54 -0400

    Scaffold TabRow

--- app/src/main/java/com/krental/scaffoldejemplo/MainActivity.kt
+++ app/src/main/java/com/krental/scaffoldejemplo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -31,6 +33,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.FloatingActionButtonElevation
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -38,6 +41,10 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -45,11 +52,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.krental.scaffoldejemplo.ui.theme.ScaffoldEjemploTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,7 +67,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScaffoldEjemploTheme {
-                ScaffoldEjemplo()
+                ScaffoldTabRow()
+                //ScaffoldCenterAlignedTopAppBar()
             }
         }
     }
@@ -66,7 +76,102 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScaffoldEjemplo() {
+fun ScaffoldTabRow() {
+    val tabTitles = listOf("Inicio","Favoritos","Perfil")
+    //Estas son las pestañas. Se recomienda de 3 a 5. Con
+    //mas pestañas es preferible un ScrollTabRow
+
+    val tabIcons = listOf(
+        Icons.Default.Home,
+        Icons.Default.Favorite,
+        Icons.Default.Person
+    )
+
+    var selectedTabIndex by rememberSaveable { mutableIntStateOf(0) }
+
+    val interactionSources = remember {
+        List(tabTitles.size) {
+            MutableInteractionSource()
+        }
+    }
+    //Lista de fuentes de interaccion (interation source)
+    //Sensor invisible que se le asigna a un componente boton, tarjeta
+    //o pestaña. Permitira detectar toques, presion prolongada o un
+    //enfoque desde el teclado. Se usa para efectos visuales,
+    //animaciones o detectar eventos de interaccion
+
+    Scaffold(
+        topBar = {
+            TabRow(
+                selectedTabIndex = selectedTabIndex,
+                modifier = Modifier.statusBarsPadding(),
+                containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                indicator = { tabPosition ->
+                    TabRowDefaults.SecondaryIndicator(
+                        modifier = Modifier
+                            .tabIndicatorOffset(tabPosition[selectedTabIndex])
+                            .height(3.dp),
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                },
+                divider = {
+                    HorizontalDivider(
+                        color = MaterialTheme.colorScheme.outlineVariant,
+                        thickness = 1.dp
+                    )
+                }
+            ){
+                tabTitles.forEachIndexed { index, title ->
+
+                    val enabled = index != 1
+
+                    Tab(
+                        selected = selectedTabIndex == index,
+                        onClick = {
+                            //selectedTabIndex = index
+                            if (enabled) selectedTabIndex = index
+                        },
+                        modifier = Modifier.padding(horizontal = 4.dp),
+                        enabled = enabled,
+                        text = {
+                            Text(
+                                text = title,
+                                fontSize = 16.sp,
+                                style = MaterialTheme.typography.titleLarge
+                            )
+                        },
+                        icon = {
+                            Icon(
+                                imageVector = tabIcons[index],
+                                contentDescription = title
+                            )
+                        },
+                        selectedContentColor = MaterialTheme.colorScheme.onPrimary,
+                        unselectedContentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        interactionSource = interactionSources[index]
+                    )
+                }
+            }
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(16.dp)
+        ){
+            when(selectedTabIndex){
+                0 -> Text("Pantalla de Inicio" , style= MaterialTheme.typography.bodyLarge)
+                1 -> Text("Pantalla de Favoritos" , style= MaterialTheme.typography.bodyLarge)
+                2 -> Text("Pantalla de Perfil" , style= MaterialTheme.typography.bodyLarge)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldCenterAlignedTopAppBar() {
     val items = listOf("Inicio","Buscar","Perfil")
     val icons = listOf(Icons.Default.Home,Icons.Default.Search,Icons.Default.Person)
     var selectedItem by rememberSaveable { mutableIntStateOf(0) }
